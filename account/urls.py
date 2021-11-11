2f9c7a7931d022fbde54564f6bdb72c68e6e3e93
sparsh-m <sparshmisra1@gmail.com>
2021-11-12 01:15:10 +0530

    added password change functionality

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='ogout'),
-    path('',views.dashboard,name='dashboard')
+    path('',views.dashboard,name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
